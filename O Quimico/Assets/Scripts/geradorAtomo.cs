01304a5850088610a7eaa298e7dc62cb36c05219
Marcelo Mota <eu.nilinho.okra@hotmail.com>
2017-08-14 19:07:48 -0300

    script de geração e destruição incompleto
    terminando script de geração e destruição de objetos

---
+++ O Quimico/Assets/Scripts/geradorAtomo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class geradorAtomo : MonoBehaviour {
+    public GameObject atomoH;
+    public float tempoDeGerar;
+    public float tempoAtual;
+
+	// Use this for initialization
+	void Start () {
+        tempoAtual = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tempoAtual += Time.deltaTime;
+        if (tempoAtual >= tempoDeGerar) {
+        }
+        tempoAtual = 0;
+        GameObject tempPrefab = Instantiate(atomoH) as GameObject;
+       
+	}
+}
