1770c8b3f2d9db4a728e3bf04a71df7f4a6c4524
Marcelo Mota <eu.nilinho.okra@hotmail.com>
2017-08-14 23:31:27 -0300

    script de geração e destruição completo
    objetos criados e destruidos, falta agora dinamica de pressionar e destruir e escrever no painel do quadro.

--- O Quimico/Assets/Scripts/geradorAtomo.cs
+++ O Quimico/Assets/Scripts/geradorAtomo.cs
@@ -3,22 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class geradorAtomo : MonoBehaviour {
-    public GameObject atomoH;
+    public GameObject[] atomos;
     public float tempoDeGerar;
     public float tempoAtual;
+    private int contador;//usado pra pegar a posicao do array
+    private int posicao;
+    private float y;
 
 	// Use this for initialization
 	void Start () {
         tempoAtual = 0;
+
+       
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
         tempoAtual += Time.deltaTime;
         if (tempoAtual >= tempoDeGerar) {
+            tempoAtual = 0;
+            posicao = Random.Range(1,85);
+            y = posicao-5f;// altera o lugar de gerar randomicamente os objetos
+            contador = Random.Range(0,6);
+            
+
+
+            GameObject tempPrefab = Instantiate(atomos[contador]) as GameObject;
+            tempPrefab.transform.position = new Vector3(transform.position.x, y, transform.position.z);
+            
         }
-        tempoAtual = 0;
-        GameObject tempPrefab = Instantiate(atomoH) as GameObject;
+        
        
 	}
 }
---
+++ O Quimico/Assets/Scripts/movimentoAtomo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentoAtomo : MonoBehaviour {
+   public Rigidbody2D corpoAtomo;
+   public float deslocamento;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        corpoAtomo.AddForce(new Vector2(deslocamento, 0));//adiciona um deslocamento a esquerda quando criado
+
+        if (transform.position.x<=-200) {//destroi o objeto criado na posição -100
+            Destruir();
+        }
+		
+	}
+    private void Destruir() {
+        Destroy(transform.gameObject);
+    }
+}
